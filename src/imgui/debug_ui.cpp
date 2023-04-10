d6c043cfc717018fb9156347f6b12b261a8c697e
maksasj <radioboos@gmail.com>
2023-04-10 20:14:43 +0300

    debug ui javadoc comments

---
+++ src/imgui/debug_ui.cpp
@@ -0,0 +1,63 @@
+#include "debug_ui.h"
+
+void omniscia::core::DebugUI::init(GLFWwindow* window) {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); 
+
+    _imgui_io = &io;
+    
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+}
+
+void omniscia::core::DebugUI::render() {
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    if(_imgui_io == nullptr) return;
+    ImGuiIO& io = *_imgui_io;
+
+    /* ImGui */
+    ImGuiWindowFlags window_flags = 
+        ImGuiWindowFlags_NoDecoration | 
+        ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_AlwaysAutoResize | 
+        ImGuiWindowFlags_NoSavedSettings | 
+        ImGuiWindowFlags_NoFocusOnAppearing | 
+        ImGuiWindowFlags_NoNav;
+
+    ImGui::SetNextWindowPos({10.0f, 10.0f}, 0, {0.0f, 0.0f});
+    ImGui::SetNextWindowBgAlpha(0.35f);
+    if (ImGui::Begin("Example: Simple overlay", nullptr, window_flags)) {
+        
+        ImGui::Text("Frames buffered %llu / %llu", _metrics._timeCurrentLineLength, _metrics._timeMaxLineLength);
+        ImGui::Text("Time manipulation time %f [ms]", _metrics._timeManipulationTime);
+        ImGui::Separator();
+
+        ImGui::Text("Application average %.3f [ms/frame] (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        ImGui::Separator();
+        
+        ImGui::Text("Currently playing %d/64 [sounds]", _metrics._currentlyPlayingSoundCount);
+        ImGui::Separator();
+
+        ImGui::Text("Player position: (x: %.3f, y: %.3f)", _metrics._playerPos.x, _metrics._playerPos.y);
+        ImGui::Separator();
+
+        ImGui::End();
+    }
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+omniscia::core::DebugUI::DebugMetrics& omniscia::core::DebugUI::get_metrics() {
+    return _metrics;
+};
+
+omniscia::core::DebugUI& omniscia::core::DebugUI::get_instance() {
+    static DebugUI instance;
+    return instance;
+}
--- src/imgui/debug_ui.h
+++ src/imgui/debug_ui.h
@@ -1,3 +1,14 @@
+/**
+ * @file 
+ * debug_ui.h
+ * 
+ * @author 
+ * Maksim Jaroslavcevas radioboos@gmail.com
+ * 
+ * @copyright
+ * see LICENSE.md file
+*/
+
 #ifndef _DEBUG_GUI_
 #define _DEBUG_GUI_
 
@@ -11,95 +22,75 @@
 #include "gfx.h"
 
 namespace omniscia::core {
+    /**
+     * @brief DebugUI - singleton class used for debugging, and collecting different metrics across entire game
+    */
     class DebugUI {
         private:
+            /**
+             * @brief DebugMetrics - class that stores diffrent metrics
+            */
             struct DebugMetrics {
+                /** @brief How much milliseconds code that manipulates time line is took executing */
                 f32 _timeManipulationTime;
-
+                
+                /** @brief Time line length */
                 u64 _timeCurrentLineLength;
+                
+                /** @brief Time max line length */
                 u64 _timeMaxLineLength;
+
+                /** @brief How much sounds is currently playing */
                 i32 _currentlyPlayingSoundCount;
+                
+                /** @brief Is game in time jump mode */
                 bool _isTimeJump;
 
+                /** @brief Current player position */
                 Vec2f _playerPos;
             };
 
+            /** @brief Pointer to imgui io */
             ImGuiIO* _imgui_io;    
 
+            /** @brief Metrics it self */
             DebugMetrics _metrics;
 
+            /** @brief Hidden constructor */
             DebugUI() : _imgui_io(nullptr) {};
-            DebugUI(DebugUI const&) {};
-            void operator=(DebugUI const&) {};
-        public:
-            void init(GLFWwindow* window) {
-                IMGUI_CHECKVERSION();
-                ImGui::CreateContext();
-                ImGuiIO& io = ImGui::GetIO(); 
-
-                _imgui_io = &io;
-                
-                ImGui::StyleColorsDark();
-                ImGui_ImplGlfw_InitForOpenGL(window, true);
-                ImGui_ImplOpenGL3_Init("#version 330");
-            }
-
-            void render() {
-                ImGui_ImplOpenGL3_NewFrame();
-		        ImGui_ImplGlfw_NewFrame();
-		        ImGui::NewFrame();
-
-                if(_imgui_io == nullptr) return;
-                ImGuiIO& io = *_imgui_io;
-
-                /* ImGui */
-                ImGuiWindowFlags window_flags = 
-                    ImGuiWindowFlags_NoDecoration | 
-                    ImGuiWindowFlags_NoMove |
-                    ImGuiWindowFlags_AlwaysAutoResize | 
-                    ImGuiWindowFlags_NoSavedSettings | 
-                    ImGuiWindowFlags_NoFocusOnAppearing | 
-                    ImGuiWindowFlags_NoNav;
-
-                ImGui::SetNextWindowPos({10.0f, 10.0f}, 0, {0.0f, 0.0f});
-                ImGui::SetNextWindowBgAlpha(0.35f);
-                if (ImGui::Begin("Example: Simple overlay", nullptr, window_flags)) {
-                    
-                    ImGui::Text("Frames buffered %llu / %llu", _metrics._timeCurrentLineLength, _metrics._timeMaxLineLength);
-                    ImGui::Text("Time manipulation time %f [ms]", _metrics._timeManipulationTime);
-                    ImGui::Separator();
-
-                    ImGui::Text("Application average %.3f [ms/frame] (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-                    ImGui::Separator();
-                    
-                    ImGui::Text("Currently playing %d/64 [sounds]", _metrics._currentlyPlayingSoundCount);
-                    ImGui::Separator();
-
-                    ImGui::Text("Player position: (x: %.3f, y: %.3f)", _metrics._playerPos.x, _metrics._playerPos.y);
-                    ImGui::Separator();
-
-                    /*
-                    if (ImGui::IsMousePosValid())
-                        ImGui::Text("Mouse Position: (%.1f,%.1f)", io.MousePos.x, io.MousePos.y);
-                    else
-                        ImGui::Text("Mouse Position: <invalid>");
-                    */
-
-                    ImGui::End();
-                }
-
-                ImGui::Render();
-                ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-            }
-
-            DebugMetrics& get_metrics() {
-                return _metrics;
-            };
             
-            static DebugUI& get_instance() {
-                static DebugUI instance;
-                return instance;
-            }
+            /** @brief Hidden copy constructor */
+            DebugUI(DebugUI const&) {};
+            
+            /** @brief Hidden assignment operator */
+            void operator=(DebugUI const&) {};
+
+        public:
+            /**
+             * @brief Initializes DebugUI instance
+             * 
+             * @param window pointer to GLFW windows instance 
+            */
+            void init(GLFWwindow* window);
+
+            /**
+             * @brief Renders DebugUI on to the screen
+            */
+            void render();
+            
+            /**
+             * @brief References current metrics object
+             * 
+             * @return reference to the metrics object instance
+             */
+            DebugMetrics& get_metrics();
+            
+            /**
+             * @brief Get the singleton instance of the DebugUI
+             * 
+             * @return Reference to singleton instance of the DebugUI
+            */
+            static DebugUI& get_instance();
     };
 }
 
