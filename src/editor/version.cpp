ac9f0657438a6535704b5badc4a5e445c686cea1
maksasj <radioboos@gmail.com>
2023-07-10 23:04:08 +0300

    deleted megaboost submodule as well as little cmake code refactoring

--- src/editor/version.cpp
+++ src/editor/version.cpp
@@ -1,6 +1,6 @@
 #include "version.h"
 
-#define _OMNISCIA_EDITOR_VERSION_ 1498
+#define _OMNISCIA_EDITOR_VERSION_ 1499
 
 unsigned long long get_editor_version() {
     return _OMNISCIA_EDITOR_VERSION_;
--- src/game/version.cpp
+++ src/game/version.cpp
@@ -1,6 +1,6 @@
 #include "version.h"
 
-#define _OMNISCIA_VERSION_ 5118
+#define _OMNISCIA_VERSION_ 5120
 
 unsigned long long get_game_version() {
     return _OMNISCIA_VERSION_;
