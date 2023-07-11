9ddb39657efd199559d69503c29320a9186d4d02
maksasj <radioboos@gmail.com>
2023-07-11 19:47:18 +0300

    basic file saving

--- src/editor/systems/level_manager.cpp
+++ src/editor/systems/level_manager.cpp
@@ -9,7 +9,17 @@ void omniscia_editor::systems::LevelManager::file_open() {
 }
 
 void omniscia_editor::systems::LevelManager::file_save() {
+    using namespace omni::reflector::serialization;
 
+    ofstream file;
+    file.open ("level.json");
+    
+    const auto jsonObject = JsonSerializer::json_serialize(_levelData);
+    const auto stringRepresentation = jsonObject.dump(4);
+
+    file << stringRepresentation;
+
+    file.close();
 }
 
 omniscia_editor::systems::LevelManager& omniscia_editor::systems::LevelManager::get_instance() {
